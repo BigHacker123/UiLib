a0f5950faa86ce1077756207535989b1db6f446c
BigHacker123 <116747999+BigHacker123@users.noreply.github.com>
2023-02-05 18:52:51 +0000

    Update .lua

--- .lua
+++ .lua
@@ -1,7 +1,5 @@
---[[ Credits
-    Matas#3535 @matas - Created UI
-    bored#9316 @wally hub user - Helped make library
-]]
+--// beamed.solutions!
+
 -- // Variables
 local ws = game:GetService("Workspace")
 local uis = game:GetService("UserInputService")
@@ -1461,97 +1459,6 @@ local GameName = game:GetService("MarketplaceService"):GetProductInfo(game.Place
             --
             window.cursor["cursor"].Transparency = window.isVisible and 1 or 0
             window.cursor["cursor_inline"].Transparency = window.isVisible and 1 or 0
-			
-			local lines = {}
-        --
-        function window:Crosshair()
-            --
-            local dot = utility:new("Square",{
-                Visible =  true,
-                Size = Vector2.new(getgenv().Cursor.DotSize, getgenv().Cursor.DotSize),
-                Color = getgenv().Cursor.Color,
-                Filled = true,
-                ZIndex = 2,
-                Transparency = 1,
-            })
-            --
-            local outline = utility:new("Square",{
-                Visible =  true,
-                Size = Vector2.new(4, 4),
-                Color = Color3.fromRGB(0, 0, 0),
-                Filled = true,
-                ZIndex = 1,
-                Transparency = 1
-            })
-            --
-            for i=1 , 4 do
-                local line = utility:new("Line",{
-                    Visible =  true,
-                    From = Vector2.new(200,500),
-                    To = Vector2.new(200,500),
-                    Color = getgenv().Cursor.Color,
-                    Thickness = getgenv().Cursor.Thickness,
-                    ZIndex = 2,
-                    Transparency = 1
-                })
-                --
-                local line_outline = utility:new("Line",{
-                    Visible =  true,
-                    From = Vector2.new(200,500),
-                    To = Vector2.new(200,500),
-                    Color = Color3.fromRGB(0, 0, 0),
-                    Thickness = getgenv().Cursor.Thickness + 2.5,
-                    ZIndex = 1,
-                    Transparency = 1
-                })
-                --
-                lines[i] = {line, line_outline}
-            end
-
-            local angle = 0
-
-            local connection = rs.RenderStepped:connect(function()
-                if getgenv().Cursor.Enabled then
-                    local pos = Vector2.new(game.Players.LocalPlayer:GetMouse().X, game.Players.LocalPlayer:GetMouse().Y + game:GetService("GuiService"):GetGuiInset().Y)
-                    angle = angle + (1 / (getgenv().Cursor.Speed * 10))
-                    --
-                    dot.Visible = true
-                    dot.Color = getgenv().Cursor.Color
-                    dot.Position = Vector2.new(pos.X - 1, pos.Y - 1)
-                    dot.Size = Vector2.new(getgenv().Cursor.DotSize, getgenv().Cursor.DotSize)
-                    --
-                    outline.Visible = getgenv().Cursor.Outline
-                    outline.Position = Vector2.new(pos.X - 2, pos.Y - 2)
-                    --
-                    for index, line in pairs(lines) do
-                        index = index
-                        x = {pos.X + (math.cos(angle + (index * (math.pi / 2))) * (getgenv().Cursor.Radius + ((getgenv().Cursor.Radius * math.sin(angle)) / 9))), pos.X + (math.cos(angle + (index * (math.pi / 2))) * ((getgenv().Cursor.Radius - 20) - (((getgenv().Cursor.Radius - 20) * math.cos(angle)) / 4)))}
-                        y = {pos.Y + (math.sin(angle + (index * (math.pi / 2))) * (getgenv().Cursor.Radius + ((getgenv().Cursor.Radius * math.sin(angle)) / 9))), pos.Y + (math.sin(angle + (index * (math.pi / 2))) * ((getgenv().Cursor.Radius - 20) - (((getgenv().Cursor.Radius - 20) * math.cos(angle)) / 4)))}
-                        --
-                        line[1].Visible = true
-                        line[1].Color = getgenv().Cursor.Color
-                        line[1].From = Vector2.new(x[2], y[2])
-                        line[1].To = Vector2.new(x[1], y[1])
-                        line[1].Thickness = getgenv().Cursor.Thickness
-                        --
-                        line[2].Visible = getgenv().Cursor.Outline
-                        line[2].From = line[1].From
-                        line[2].To = line[1].To
-                        line[2].Thickness = getgenv().Cursor.Thickness + 2.5
-                    end
-                    uis.MouseIconEnabled = false
-                else
-                    dot.Visible = false
-                    outline.Visible = false
-                    --
-                    for index, line in pairs(lines) do
-                        line[1].Visible = false
-                        line[2].Visible = false
-                    end
-                    uis.MouseIconEnabled = true
-                end
-            end)
-        end
 
             if window.isVisible then
                 local set_back = false
